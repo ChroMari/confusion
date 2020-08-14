d9d69c47da5b01f2fa976db73084b921d7cedaf4
ChroMari <chromarija@gmail.com>
2020-08-14 13:41:15 +0300

    refactor: Изменение на компоненты представления и контейнера

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from './shared/dishes.js';
+import Main from './components/MainComponent.js';
 
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={DISHES}/>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
