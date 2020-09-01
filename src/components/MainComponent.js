678fd01174b698514390438891b5fe1b6f09fe15
ChroMari <chromarija@gmail.com>
2020-09-01 12:06:47 +0300

    refactor

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -45,6 +45,7 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
+
              <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
         comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
         addComment={this.props.addComment}
@@ -58,10 +59,10 @@ class Main extends Component {
         <div>
           <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
         </div>
